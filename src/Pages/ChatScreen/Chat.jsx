dfeb502d08a06e973ee180db9474893382b901ed
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-02-19 19:32:34 +0530

    Fixed some bugs

--- src/Pages/ChatScreen/Chat.jsx
+++ src/Pages/ChatScreen/Chat.jsx
@@ -19,35 +19,37 @@ bottom: 15px;
 position: absolute;
 width: 80%;
 height: 5%;
+margin-left: 2%;
+margin-right: auto;
+display: flex;
 `
 
 const imageStyle = {
   "width": "25%",
   "height": "90%",
-  "z-index": 10,
-  "position": "sticky",
-  "background": "black"
 }
 
 const ChatBox = style.textarea`
-    background: white;
-    width: 100%;
-    padding: 15px;
-    border-left: 0.5px solid grey;
-    border-radius: 50px;
-    bottom: 15px;
-    position: absolute;
-    margin-left: 2%;
-    margin-right: auto;
-    box-shadow: 0 5px 10px 0 gray, 0 0 40px 0 gray;
-    &:focus {
-      outline: none;
-    } 
+background: white;
+width: 100%;
+padding: 15px;
+border-left: 0.5px solid grey;
+border-radius: 50px;
+bottom: 15px;
+position: absolute;
+box-shadow: 0 5px 10px 0 gray, 0 0 40px 0 gray;
+&:focus {
+  outline: none;
+} 
 `;
 
 const SendButton = style.button`
-  border: none;
-  background: none;
+border: none;
+background: black;
+z-index: 10px;
+"position": "sticky",
+"background": "black",
+"bottom": "50%"
 `;
 
 export default Chat
